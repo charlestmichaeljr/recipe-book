599b3b35ad8c7d225d9e4f0f14d334c628d6c8f5
Charles Michael Jr <37586218+charlestmichaeljr@users.noreply.github.com>
2018-11-27 15:29:28 -0500

    Added components and laid out UI for each component.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('Apple Pie',
+      'tasty dessert',
+      'https://cdn.vox-cdn.com/thumbor/esKKy31RBlU5AyaHuPuggsqJvMo=/0x0:2039x1529/1400x1400/filters:focal(0x0:2039x1529):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/47743599/fruit_pie.0.jpg'),
+    new Recipe('Pumpkin Pie',
+      'Not so great',
+      'https://cdn.vox-cdn.com/thumbor/esKKy31RBlU5AyaHuPuggsqJvMo=/0x0:2039x1529/1400x1400/filters:focal(0x0:2039x1529):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/47743599/fruit_pie.0.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
